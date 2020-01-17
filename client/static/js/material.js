a05ed7113f9b374071e9c179782ae762ed0e9564
Mitko Nikov <mitkonikov01@gmail.com>
2020-01-17 17:08:38 +0100

    character count for the create post field

--- client/static/js/material.js
+++ client/static/js/material.js
@@ -1,7 +1,7 @@
 import {MDCRipple} from '@material/ripple/index';
 import {MDCFloatingLabel} from '@material/floating-label';
 import {MDCLineRipple} from '@material/line-ripple';
-import {MDCTextField} from '@material/textfield';
+import {MDCTextField, MDCTextFieldFoundation} from '@material/textfield';
 import {MDCTextFieldCharacterCounter} from '@material/textfield/character-counter';
 
 $(document).ready(() => {
@@ -9,15 +9,15 @@ $(document).ready(() => {
   const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
     return new MDCRipple(el);
   });
-
+  
   const textEls = Array.from(document.querySelectorAll('.mdc-text-field'));
   textEls.forEach((el) => new MDCTextField(el));
+
+  const characterCounter = new MDCTextFieldCharacterCounter(document.querySelector('.mdc-text-field-character-counter'));
 
   const floatingLabels = Array.from(document.querySelectorAll('.mdc-floating-label'));
   floatingLabels.forEach((el) => new MDCFloatingLabel(el));
 
   const lineRipples = Array.from(document.querySelectorAll('.mdc-line-ripple'));
   lineRipples.forEach((el) => new MDCLineRipple(el));
-
-  const characterCounter = new MDCTextFieldCharacterCounter(document.querySelector('.mdc-text-field-character-counter'));
 });
